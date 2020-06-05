0bcfc0dadc99df2ae8e200a4eddd34969e8420c2
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2020-06-05 13:08:34 -0600

    added first (untested) version of ppipe_merger

---
+++ ppipe_merger.c
@@ -0,0 +1,24 @@
+#include "ppipe_merger.h"
+
+void *ppipe_merge(void *inptr) {
+    struct thread_merger *merger = inptr;
+    struct circarr c = init_circarr(20, p->member_size, merger->indexer);
+    bool closed = false;
+    int i=0;
+    int j=0;
+    
+    while (!closed) {
+        ppipe_read(p, &i, &closed);
+        if (closed) {
+            break;
+        }
+        circarr_add(&c, &i);
+        while (circarr_poppable(c)) {
+            circarr_pop(&c, &j);
+        }
+    }
+    while (circarr_poppable(c)) {
+        circarr_pop(&c, &j);
+    }
+    free_circarr(c);
+}
