ce1977f9a74942290c02504c1788a6d403c10227
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2020-06-08 10:04:06 -0600

    fixed problem where outbuf and inbuf were incorrectly referenced during memcpy

--- ppipe_merger.c
+++ ppipe_merger.c
@@ -1,28 +1,34 @@
 #include "ppipe_merger.h"
+#include "circarr.h"
+#include <stdlib.h>
 
 void *ppipe_merge(void *inptr) {
     struct ppipe_merger *merger = inptr;
     struct circarr c = init_circarr(20, merger->p->member_size, merger->indexer);
     bool closed = false;
-    int i=0;
-    int j=0;
+    printf("merger->p->member_size: %ld\n", merger->p->member_size);
+    char *inbuf = calloc(1, merger->p->member_size);
+    char *outbuf = calloc(1, merger->p->member_size);
     
     while (!closed) {
-        ppipe_read(merger->p, &i, &closed);
+        ppipe_read(merger->p, inbuf, &closed);
         if (closed) {
             break;
         }
-        circarr_add(&c, &i);
+        circarr_add(&c, inbuf);
         while (circarr_poppable(c)) {
-            circarr_pop(&c, &j);
-            ppipe_write(merger->op, &j, false);
+            circarr_pop(&c, outbuf);
+            ppipe_write(merger->op, outbuf, false);
         }
     }
     while (circarr_poppable(c)) {
-        circarr_pop(&c, &j);
-        ppipe_write(merger->op, &j, false);
+        circarr_pop(&c, outbuf);
+        ppipe_write(merger->op, outbuf, false);
     }
-    ppipe_write(merger->op, &j, true);
+    ppipe_write(merger->op, outbuf, true);
+    printf("c.buf: %p; c.full: %p; inbuf: %p; outbuf: %p", c.buf, c.full, inbuf, outbuf);
     circarr_free(c);
-    return(inptr);
+    free(inbuf);
+    free(outbuf);
+    pthread_exit(NULL);
 }
